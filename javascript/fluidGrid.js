9a52fc5ed0bebb7e4ebba79dd773cf2df41b50f3
Hilding Lindén <hilding.linden@abo.fi>
2020-03-11 17:32:57 +0200

    Refactor javascript version

---
+++ javascript/fluidGrid.js
@@ -0,0 +1,174 @@
+"use strict";
+
+class FluidGrid {
+	constructor(size, diffusion, viscosity, fade, dt, iterations) {
+		this.dt = dt;
+		this.iterations = iterations;
+
+		this.size = size;
+		this.diffusionRate = diffusion;
+		this.viscosity = viscosity;
+		this.fadeRate = fade;
+
+		this.density = new Array(size*size).fill(0);
+		this.prevDensity = new Array(size*size).fill(0);
+
+		this.velocityX = new Array(size*size).fill(0);
+		this.velocityY = new Array(size*size).fill(0);
+		this.prevVelocityX = new Array(size*size).fill(0);
+		this.prevVelocityY = new Array(size*size).fill(0);
+	}
+
+	// Add density (dye) to a cell
+	addDensity(x, y, amount) {
+		this.density[x + y * this.size] += amount;
+	}
+
+	// Add velocity (in x & y direction) to a cell
+	addVelocity(x, y, amountX, amountY) {
+		this.velocityX[x+y*this.size] += amountX;
+		this.velocityY[x+y*this.size] += amountY;
+	}
+
+	step() {
+		let t0 = performance.now();
+
+		// Diffuse the velocities of the fluid (viscous diffusion)
+		diffuse(1, this.prevVelocityX, this.velocityX, this.viscosity, this.dt, this.iterations, this.size);
+		diffuse(2, this.prevVelocityY, this.velocityY, this.viscosity, this.dt, this.iterations, this.size);
+
+		// Equalize cells
+		project(this.prevVelocityX, this.prevVelocityY, this.velocityX, this.velocityY, this.iterations, this.size);
+
+		// Move the velocity of the fluid (self-advection), swapping order of arr and prev array
+		advect(1, this.velocityX, this.prevVelocityX, this.prevVelocityX, this.prevVelocityY, this.dt, this.size);
+		advect(2, this.velocityY, this.prevVelocityY, this.prevVelocityX, this.prevVelocityY, this.dt, this.size);
+
+		// Equalize cells
+		project(this.velocityX, this.velocityY, this.prevVelocityX, this.prevVelocityY, this.iterations, this.size);
+
+		// Diffuse and move the density (dye), swapping order of arr and prev array
+		diffuse(0, this.prevDensity, this.density, this.diffusionRate, this.dt, this.iterations, this.size);
+		advect(0, this.density, this.prevDensity, this.velocityX, this.velocityY, this.dt, this.size);
+
+		// Fade the dye
+		this.density = this.density.map(x => Math.max(x-this.fadeRate, 0));
+
+		let t1 = performance.now();
+		console.log("Step ", t1-t0);
+	}
+}
+
+function diffuse(direction, arr, prevArr, diffusionRate, dt, iterations, size) {
+	let diffusion = dt * diffusionRate * size * size;
+
+	// Guass-Seidel relaxation
+	for (let iteration = 0; iteration < iterations; iteration++) {
+		for (let y = 1; y < size - 1; y++) {
+			for (let x = 1; x < size - 1; x++) {
+
+				let up 	  =	arr[x		+ (y-1) * size];
+				let down  =	arr[x 		+ (y+1) * size];
+				let left  =	arr[(x-1)	+ y 	* size];
+				let right = arr[(x+1)	+ y 	* size];
+
+				arr[x+y*size] = (prevArr[x+y*size] + diffusion * (up + down + left + right)) / (1 + 4 * diffusion);
+			}
+		}
+		// Set the edges of the whole fluid
+		set_bounds(direction, arr, size);
+	}
+}
+
+function project(velocityX, velocityY, p, div, iterations, size) {
+	// Hodge decomposition, poisson equation
+
+	let h = 1/size;
+
+	for (let y = 1; y < size-1; y++) {
+		for (let x = 1; x < size-1; x++) {
+			div[x+y*size] = -0.5*h*(
+				velocityX[(x+1) + y 	* size] - velocityX[(x-1) + y 	  * size] +
+				velocityY[x     + (y+1) * size] - velocityY[x 	  + (y-1) * size]
+			);
+			p[x+y*size] = 0;
+		}
+	}
+
+	set_bounds(0, div, size);
+	set_bounds(0, p, size);	// Unnecessary?
+
+	// Gauss-Seidel relaxation
+	for (let iteration = 0; iteration < iterations; iteration++) {
+		for (let y = 1; y < size-1; y++) {
+			for (let x = 1; x < size-1; x++) {
+				let up 	  =	p[x		+ (y-1) * size];
+				let down  =	p[x 		+ (y+1) * size];
+				let left  =	p[(x-1)	+ y 	* size];
+				let right = p[(x+1)	+ y 	* size];
+
+				p[x+y*size] = (div[x+y*size] + up + down + left + right) / 4;
+			}
+		}
+		set_bounds(0, p, size);
+	}
+
+	for (let y = 1; y < size-1; y++) {
+		for (let x = 1; x < size-1; x++) {
+			velocityX[x+y*size] -= 0.5*(p[(x+1) + y 	* size] - p[(x-1) + y 	  * size])/h;
+			velocityY[x+y*size] -= 0.5*(p[x     + (y+1) * size] - p[x 	  + (y-1) * size])/h;
+		}
+	}
+
+	set_bounds(1, velocityX, size);
+	set_bounds(2, velocityY, size);
+}
+
+function advect(direction, arr, prevArr, velocityX, velocityY, dt, size) {
+	let dt0 = dt * size;
+
+	for (let y = 1; y < size - 1; y++) {
+		for (let x = 1; x < size - 1; x++) {
+			// Get the previous x and y location of current cell
+			let prevX = x - dt0 * velocityX[x+y*size];
+			let prevY = y - dt0 * velocityY[x+y*size];
+
+			// Clamp to 0.5 from edges
+			prevX = Math.min(Math.max(0.5, prevX), size-1+0.5);
+			prevY = Math.min(Math.max(0.5, prevY), size-1+0.5);
+
+			let prevXInt = Math.floor(prevX);
+			let prevYInt = Math.floor(prevY);
+
+			let s1 = prevX - prevXInt;
+			let s0 = 1 - s1;
+			let t1 = prevY - prevYInt;
+			let t0 = 1 - t1;
+
+			arr[x+y*size] =
+				s0 * (t0 * prevArr[prevXInt     + prevYInt * size] + t1 * prevArr[prevXInt     + (prevYInt+1) * size]) +
+				s1 * (t0 * prevArr[(prevXInt+1) + prevYInt * size] + t1 * prevArr[(prevXInt+1) + (prevYInt+1) * size]);
+		}
+	}
+
+	// Set the edges of the whole fluid
+	set_bounds(direction, arr, size);
+}
+
+function set_bounds(direction, arr, size) {
+	for (let i = 1; i < size; i++) {
+		// Left and right edge get the reverse velocity of the neighbor if in X direction
+		arr[0		 + i * size] = direction == 1 ? -arr[1 	  	  + i * size] : arr[1		 + i * size];
+		arr[(size-1) + i * size] = direction == 1 ? -arr[(size-2) + i * size] : arr[(size-2) + i * size];
+
+		// Top and bottom edge get the reverse velocity of the neighbor if in Y direction
+		arr[i + 0		 * size] = direction == 2 ? -arr[i + 1 	  	  * size] : arr[i + 1 	     * size];
+		arr[i + (size-1) * size] = direction == 2 ? -arr[i + (size-2) * size] : arr[i + (size-2) * size];
+	}
+
+	// The corners get the average values of their neighbors
+	arr[0        + 0 	  	* size] = 0.5 * (arr[1 		  + 0 	 	 * size] + arr[0 		+ 1 	   * size]);
+	arr[(size-1) + 0 	  	* size] = 0.5 * (arr[(size-2) + 0 	 	 * size] + arr[(size-1) + 1		   * size]);
+	arr[0 	     + (size-1) * size] = 0.5 * (arr[0 		  + (size-2) * size] + arr[1 		+ (size-1) * size]);
+	arr[(size-1) + (size-1) * size] = 0.5 * (arr[(size-2) + (size-1) * size] + arr[(size-1) + (size-2) * size]);
+}
--- javascript/main.js
+++ javascript/main.js
@@ -5,54 +5,49 @@ function main() {
 	let context = canvas.getContext("2d");
 
 	let N = 50;
-	let SCALE = 8;
+	let SCALE = canvas.width/N;
 
-	// n, diffusion, viscosity, dt
-	let grid = new FluidGrid(N, 0.00001, 0.00001, 0.05);
+	// n, diffusion, viscosity, fadeRate, dt, iterations
+	let grid = new FluidGrid(N, 0.001, 0.001, 0.001, 0.016, 4);
 
 	let velX = 0;
-	let velY = 1;
+	let velY = 0;
 
-	function draw() {
+	function loop() {
+		grid.addVelocity(N/2, N/2, velX, velY);
+		grid.addDensity(N/2, N/2, 1);
+		grid.step();
+
+		let t0 = performance.now();
+
 		context.clearRect(0, 0, canvas.width, canvas.height);
-		for (let i = 0; i < N; i++) {
-			for (let j = 0; j < N; j++) {
-				let color = 255 * grid.density[i+j*N];
+
+		for (let y = 0; y < N; y++) {
+			for (let x = 0; x < N; x++) {
+				let color = 255 * grid.density[x+y*N];
 				context.fillStyle = "rgba("+color+","+color+","+color+",255)";
-				context.fillRect(i*SCALE, j*SCALE, SCALE, SCALE);
+				context.fillRect(x*SCALE, y*SCALE, SCALE, SCALE);
 			}
 		}
-		grid.addVelocity(25,25,velX,velY);
-		grid.addDensity(25,25,1);
-		grid.stepForward();
-		window.requestAnimationFrame(draw);
+
+		let t1 = performance.now();
+		console.log("Draw ", t1-t0);
+
+		window.requestAnimationFrame(loop);
 	}
 
-	window.requestAnimationFrame(draw);
+	window.requestAnimationFrame(loop);
 
 	canvas.addEventListener("mousemove", (event) => {
-		let x = event.clientX - 210;
-		let y = event.clientY - 210;
-		let mag = Math.sqrt(x*x+y*y);
-		velX = x/mag;
-		velY = y/mag;
+		let x = event.clientX - canvas.width/2 - 10;
+		let y = event.clientY - canvas.height/2 - 10;
+		let angle = Math.atan2(y, x);
+		velX = 2 * Math.cos(angle);
+		velY = 2 * Math.sin(angle);
 	});
 
 	function scale(x, in_min, in_max, out_min, out_max) {
 		return(x - in_min) * (out_max - out_min) / (in_max - in_min) + out_min;
 	}
-	/*
-	let imageData = context.createImageData(400, 400);
-	let data = imageData.data;
-	let steps = 0;
-	function step() {
-		context.clearRect(0, 0, canvas.width, canvas.height);
-		context.putImageData(imageData, steps%400, 100);
-		steps += 2;
-		window.requestAnimationFrame(step);
-	}
-
-	window.requestAnimationFrame(step);
-	*/
 }
 
